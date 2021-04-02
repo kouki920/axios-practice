e24087c7d8f812b9304cb10d819679bb129ee6f4
kouki920 <kouki.htt.920@gmail.com>
2021-04-02 18:18:38 +0900

    [add]laravel、Vue、axiosを使いWebAPIを取得するコードを追記

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import './bootstrap'
+import Vue from 'vue'
+import Axios from './components/Axios'
+
+const axios = new Vue({
+  el:'#axios',
+  components: {
+    Axios,
+  }
+})
---
+++ laravel/resources/js/components/Axios.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="style1">
+        <h1>{{ message }}</h1>
+        <p v-for="user in users">{{ user.name.title }} {{ user.name.last }}</p>
+        <button
+      class="btn-sm shadow-none border border-primary p-2"
+      @click="clickData">
+      <font size="3">axiosを使いデータを取得</font>
+    </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           message: 'Axios',
+            users: []
+        };
+    },
+methods: {
+      clickData() {
+axios.get('https://randomuser.me/api/',
+            {
+              params: {
+                results: '5'
+              }
+            })
+        .then(response => this.users = response.data.results)
+        .catch(response => console.error(response))
+    },
+    },
+}
+</script>
